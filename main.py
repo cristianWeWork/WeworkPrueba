b4f47e46ee07f64941b640080c99aa40b1c0e59b
Cristian Maciel~ <cristian@weworkfactory.com>
2023-09-12 09:18:00 +0200

    main.py changes

--- main.py
+++ main.py
@@ -3,41 +3,33 @@ from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import uvicorn
+from pydantic import BaseModel
 
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
-    print('Request for index page received')
-    return templates.TemplateResponse('index.html', {"request": request})
+class Item(BaseModel):
+    user: str
+    name: str | None = None
 
-@app.get('/favicon.ico')
-async def favicon():
-    file_name = 'favicon.ico'
-    file_path = './static/' + file_name
-    return FileResponse(path=file_path, headers={'mimetype': 'image/vnd.microsoft.icon'})
 
-@app.post('/hello', response_class=HTMLResponse)
-async def hello(request: Request, name: str = Form(...)):
-    if name:
-        print('Request for hello page received with name=%s' % name)
-        return templates.TemplateResponse('hello.html', {"request": request, 'name':name})
-    else:
-        print('Request for hello page received with no name or blank name -- redirecting')
-        return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
 @app.get("/")
 async def prueba():
     return {"message": "Hello World"}
 
-
+@app.post("/user/")
+async def create_item(item: Item):
+    frase = "Muy buenas, " + str(item.name) + ", tu nombre de usuario es: " + str(item.user)
+    return {"message": frase}
+    
 @app.get("/superhero/{superHero}")
 async def superheroCall(superHero:str):
     frase = "Muy buenas, " + str(superHero).capitalize()
     
     return {"message" : str(frase)}
+
 if __name__ == '__main__':
     uvicorn.run('myapp:app', host='0.0.0.0', port=8000)
 
